0b82ae6f609938927309a4ad9b45a81fea4600cb
Kartikay Shandil <kartikays@sahaj.ai>
2023-05-04 20:20:55 +0530

    Remove actual answers

--- src/main/kotlin/com/nano/MathController.kt
+++ src/main/kotlin/com/nano/MathController.kt
@@ -2,12 +2,10 @@ package com.nano
 
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
-import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.validation.Validated
-import javax.validation.constraints.NotNull
 
 @Controller("/calculator")
 @Validated
@@ -15,16 +13,16 @@ class MathController {
 
     @Get("/greeting")
     fun greeting(): HttpResponse<String> {
-        return HttpResponse.ok("Hello World!")
+        return HttpResponse.ok("")
     }
 
     @Post("/add", produces = [MediaType.APPLICATION_JSON])
-    fun add(@Body @NotNull numbers: Numbers): HttpResponse<String> {
-        return HttpResponse.ok("""{"result": ${numbers.first + numbers.second} }""")
+    fun add(): HttpResponse<String> {
+        return HttpResponse.ok("")
     }
 
     @Post("/subtract", produces = [MediaType.APPLICATION_JSON])
-    fun subtract(@Body @NotNull numbers: Numbers): HttpResponse<String> {
-        return HttpResponse.ok("""{"result": ${numbers.first - numbers.second} }""")
+    fun subtract(): HttpResponse<String> {
+        return HttpResponse.ok("")
     }
 }
