e9a02028f0bf42b2988c03ca2bf331cd7608374d
Kartikay Shandil <kartikays@sahaj.ai>
2023-05-25 23:32:57 +0530

    Remove actual answers

--- server.js
+++ server.js
@@ -10,18 +10,16 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('Hello World!');
+    return res.send('');
 });
 
 baseRouter.post('/add', (req, res) => {
-    const { first, second } = req.body;
-    res.json({ "result": first + second });
+    res.json({ "": null });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    const { first, second } = req.body;
-    res.json({ "result": first - second });
+    res.json({ "": null });
 });
 
 app.use(baseUrl, baseRouter);
