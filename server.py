474b33d813926eab7c5b18a5c62eac0cb1f90ecd
Pranav Bhaskar <pranavb@sahaj.ai>
2023-05-25 23:20:21 +0530

    Implement add and subtract

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
 
 app = Flask(__name__)
 
@@ -6,6 +12,18 @@ app = Flask(__name__)
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
     return 'Hello world!'
+
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
+
+@app.route("/calculator/subtract", methods=['POST'])
+def subtract():
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
