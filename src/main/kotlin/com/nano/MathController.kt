b4dbead972739c60ffc6b6907b801b6e5a7cfa4e
Pranav Bhaskar <pranavb@sahaj.ai>
2023-05-25 22:35:35 +0530

    Add Body annotation

--- src/main/kotlin/com/nano/MathController.kt
+++ src/main/kotlin/com/nano/MathController.kt
@@ -3,6 +3,7 @@ package com.nano
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.MutableHttpResponse
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
@@ -18,12 +19,12 @@ class MathController {
     }
 
     @Post("/add", produces = [MediaType.APPLICATION_JSON])
-    fun add(numbers: Numbers): MutableHttpResponse<Result> {
+    fun add(@Body numbers: Numbers): MutableHttpResponse<Result> {
         return HttpResponse.ok(Result(numbers.first + numbers.second))
     }
 
     @Post("/subtract", produces = [MediaType.APPLICATION_JSON])
-    fun subtract(numbers: Numbers): HttpResponse<Result> {
+    fun subtract(@Body numbers: Numbers): HttpResponse<Result> {
         return HttpResponse.ok(Result(numbers.first - numbers.second))
     }
 }
