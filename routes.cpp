91a4bf56feca2a77a59df794dc382638c84e32f3
Kartikay Shandil <kartikays@sahaj.ai>
2023-05-26 00:54:52 +0530

    Remove actual answers

--- routes.cpp
+++ routes.cpp
@@ -2,19 +2,15 @@
 
 crow::response greet()
 {
-    return crow::response{"Hello World!"};
+    return crow::response{""};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    crow::json::wvalue result;
-    result["result"] = input["first"].i() + input["second"].i();
-    return result;
+    return crow::response{""};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    crow::json::wvalue result;
-    result["result"] = input["first"].i() - input["second"].i();
-    return result;
+    return crow::response{""};
 }
