9bb57b0845b8e70fad71a9c49f5da065bccecd11
Nandeesh-mod <84384887+Nandeesh-mod@users.noreply.github.com>
2023-05-29 21:10:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,18 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    res.json({ "result": first - second });
 });
 
 app.use(baseUrl, baseRouter);
