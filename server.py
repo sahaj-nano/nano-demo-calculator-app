bbe5b0558665617faf256ee40cb0df93435e99d2
Atharva <atv23shukla@gmail.com>
2023-05-31 22:14:08 +0530

    fixed using python

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    req = request.get_json()
+    data1 = req['first']
+    data2 = req['second']
+    result = data1 + data2
+    return jsonify({'result': result})
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    req = request.get_json()
+    data1 = req['first']
+    data2 = req['second']
+    result = data1 - data2
+    return jsonify({'result': result})
+
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
