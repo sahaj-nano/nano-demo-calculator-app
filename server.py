782aba477e271bdde90717be52935d25fa935503
saurabhdeo7 <saurabhdeo7@gmail.com>
2023-07-14 18:44:30 +0530

    SAHAJ Jhalle

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request,jsonify
 
 app = Flask(__name__)
 
@@ -10,15 +10,18 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    first = int(request.args.get('first'))
-    second = int(request.args.get('second'))
-    return { "result": first+second }
-    #return "Hello World"
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = int(first) + int(second)
+    return { "result": result }
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    first = int(request.args.get('first'))
-    second = int(request.args.get('second'))
-    return { "result": first-second }
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = int(first) - int(second)
+    return { "result": result }
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
