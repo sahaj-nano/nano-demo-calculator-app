c9fff554428fcfa782ee36ef94d14daab0a623ce
janettrez7 <treasahelosia7@gmail.com>
2023-07-15 00:57:00 +0530

    error check

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return 'Hello world!', 200
+    return 'Hello', 200
 
 
 @app.route('/calculator/add', methods=['POST'])
