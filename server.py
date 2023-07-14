e44e081c583148afdb00f66e77b3f47fc36d2b4c
saurabhdeo7 <saurabhdeo7@gmail.com>
2023-07-14 13:09:08 +0530

    Testing Changes!

--- server.py
+++ server.py
@@ -8,12 +8,16 @@ def greeting():
     return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
-def add(first,second):
-    return first+second
+def add():
+    first = Flask.request.args.get('first')
+    second = Flask.request.args.get('second')
+    return Flask.jsonify({'data':f'<p>The result is: {first+second}</p>'})
 
 @app.route("/calculator/subtract", methods=['POST'])
-def subtract(first,second):
-    return first-second
+def subtract():
+    first = Flask.request.args.get('first')
+    second = Flask.request.args.get('second')
+    return Flask.jsonify({'data':f'<p>The result is: {first-second}</p>'})
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
