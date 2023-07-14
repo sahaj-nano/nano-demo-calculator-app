a5c2fa02ca11bbedfc33a27c024048ea19219848
123jadhavyogesh <123jadhavyogesh@gmail.com>
2023-07-15 02:53:01 +0530

    yogesh submission at 2:53

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(_name_)
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return 'Hello world!', 200
+    return 'Hello world!',200
 
 @app.route('/calculator/add', methods=['POST'])
 def add():
@@ -12,7 +12,7 @@ def add():
     first = data['first']
     second = data['second']
     result = first + second
-    return jsonify({'result': result}), 200
+    return jsonify({'result': result}),200
 
 @app.route('/calculator/subtract', methods=['POST'])
 def subtract():
