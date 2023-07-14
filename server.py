75c9bc2bfdd9a88d5a0d4f7d28179893a5108342
Mriganki05 <mrigankiagarwal@gmail.com>
2023-07-15 01:41:36 +0530

    practice demo

--- server.py
+++ server.py
@@ -9,11 +9,19 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data=request.get_json()
+    first=data['first']
+    second=data['second']
+    result=first+second
+    return jsonify({'result':result}),200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data=request.get_json()
+    first=data['first']
+    second=data['second']
+    result=first-second
+    return jsonify({'result':result}),200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
