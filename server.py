c7a0bc505642da9d882ade6044826597a4e2a2ad
ShivamDandavate <shivamdandavate45@gmail.com>
2023-07-14 22:27:35 +0530

    modified server.py according to problem statement

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, request
 
 app = Flask(__name__)
-
-
+@app.route("/")
+def calculator():
+    return render_template("index.html")
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello World !!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 + num2
+    return {'result': result}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 - num2
+    return {'result': result}
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
