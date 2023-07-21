e6bf982e7ec52c6c7b1ef8975bba196ddedd19bc
Peg5u5 <92622175+Peg5u5@users.noreply.github.com>
2023-07-22 01:45:27 +0530

    Update routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,29 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"}.code(crow::HTTPStatus::OK); 
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+   int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first + second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first - second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
