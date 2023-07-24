46bbd6fabe3aa942c2d6438d91754062a72e2fcc
Adikverma <adityakumarv333@gmail.com>
2023-07-25 00:10:24 +0530

    did the basic required api contract

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -7,19 +8,27 @@ const baseUrl = '/calculator'
 
 app.use(express.json());
 
+app.use(morgan("tiny"));
+
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    res.status(200).send("Hello world!");
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    let numbers=req.body;
+    let num1= numbers.first;
+    let num2= numbers.second;
+    res.status(200).json({"result":num1+num2});
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    let numbers=req.body;
+    let num1= numbers.first;
+    let num2= numbers.second;
+    res.status(200).json({"result":num1-num2});
 });
 
 app.use(baseUrl, baseRouter);
