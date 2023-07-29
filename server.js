acb5d3cd9e5f84631cb1089a93252b05d350c391
shankarvarat <shankarvarat@gmail.com>
2023-07-29 11:39:39 +0530

    Testing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Welcome!');
 });
 
 baseRouter.post('/add', (req, res) => {
