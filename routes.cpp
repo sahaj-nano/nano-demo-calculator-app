7481b939d90f08c0c8bbb4afb16c62d010ed10ff
DHRUV RATHI <rathidhruv9285@gmail.com>
2023-08-03 14:21:58 +0530

    Update routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,15 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    return crow::response{input.first + input.second};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    return crow::response{input.first - input.second};
 }
