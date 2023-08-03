6d6b14b19f90301552c45fa5ad16bf3b10fce62b
dev <devcocid@gmail.com>
2023-08-04 01:29:29 +0530

    done the assignment

--- server.py
+++ server.py
@@ -1,19 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!', 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first + second
+        return jsonify({"result": result}), 200
+    else:
+        return jsonify({"error": "Provide both 'first' and 'second' numbers"}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first - second
+        return jsonify({"result": result}), 200
+    else:
+        return jsonify({"error": "Provide both 'first' and 'second' numbers"}), 400
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
