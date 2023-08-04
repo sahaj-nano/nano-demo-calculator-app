b4b116800f933d00a1fd1d4c91dc7ba7d5352014
Samarpitaa <81858442+Samarpitaa@users.noreply.github.com>
2023-08-04 07:50:44 +0000

    api

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/calculator/greeting', methods=['GET'])
+def greeting():
+    return "Hello world!", 200
+
+@app.route('/calculator/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = first + second
+    return jsonify({"result": result}), 200
+
+@app.route('/calculator/subtract', methods=['POST'])
+def subtract():
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = first - second
+    return jsonify({"result": result}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
