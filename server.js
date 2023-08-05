21e7fabd5c79abd3a7e5302737cdcab4e7cdea3e
Ritik Raj <ritik2358@gmail.com>
2023-08-05 15:51:29 +0530

    modifies endpoints

--- server.js
+++ server.js
@@ -10,16 +10,27 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both first and second must be numbers.' });
+    }
+
+    const result = first + second;
+    res.status(200).json({ result: result });
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both first and second must be numbers.' });
+    }
+
+    const result = first - second;
+    res.status(200).json({ result: result });
 });
 
 app.use(baseUrl, baseRouter);
