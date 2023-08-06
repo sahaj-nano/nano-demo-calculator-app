2a51247825cd398255a2fcd8e392db70628a117e
SaI <saisantosh.m02@gmail.com>
2023-08-06 15:14:35 +0530

    task done

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.status(200).send('Hello World');
+    return res.status(200).send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
