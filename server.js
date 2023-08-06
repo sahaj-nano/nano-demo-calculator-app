9fdcc2388020044735e4742dbd8a8f60e2f96f73
kalyan <kalyanram36117@gmail.com>
2023-08-06 21:43:45 +0530

    first

--- server.js
+++ server.js
@@ -3,23 +3,40 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = '/calculator';
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+   return res.send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    if (typeof first === 'number' && typeof second === 'number') {
+        const result = first + second;
+       return res.json({ result });
+    } else {
+       return res.status(400).json({ error: 'Invalid input. Both numbers must be valid.' });
+    }
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    
+    const { first, second } = req.body;
+
+   
+    if (typeof first === 'number' && typeof second === 'number') {
+       
+        const result = first - second;
+        
+        return res.json({ result });
+    } else {
+       
+        return res.status(400).json({ error: 'Invalid input. Both numbers must be valid.' });
+    }
 });
 
 app.use(baseUrl, baseRouter);
