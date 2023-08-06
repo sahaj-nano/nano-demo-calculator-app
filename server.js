1e6c03c7bbcd41da220d5e687e300f357f334e4a
i-am-sai <saiswaroopkdevarmane@gmail.com>
2023-08-06 22:02:16 +0530

    as

--- server.js
+++ server.js
@@ -3,23 +3,36 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = '/calculator';
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello world!');
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
-});
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Both "first" and "second" should be numbers.' });
+    }
+
+    const result = first - second;
+    return res.json({ result: result });
+});
+
+baseRouter.post('/add', (req, res) => {
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Both "first" and "second" should be numbers.' });
+    }
+
+    const result = first + second;
+    return res.json({ result: result });
 });
 
 app.use(baseUrl, baseRouter);
