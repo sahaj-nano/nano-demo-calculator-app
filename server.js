089696238e2a14ee6fac4d6c8ec2b4720752cda4
rithikrajyt <rrpandey855@gmail.com>
2023-08-06 20:48:53 +0530

    Tested and Changed

--- server.js
+++ server.js
@@ -3,26 +3,40 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = '/calculator';
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both values should be numbers.' });
+    }
+    
+    const result = first + second;
+    res.json({ result });
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both values should be numbers.' });
+    }
+
+    const result = first - second;
+    res.json({ result });
 });
 
 app.use(baseUrl, baseRouter);
+
 app.listen(PORT, () => {
     console.log("Server running at PORT", PORT);
 });
