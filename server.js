fcdda0380acad0d9de551ad312c02c708da9b925
NateNear <askharindrareddy@gmail.com>
2023-08-06 15:03:45 +0530

    task done

--- server.js
+++ server.js
@@ -10,16 +10,25 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    res.status(200).res.send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both first and second must be numbers.' });
+    }
+    const result = first + second;
+    return res.json({ result });
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both first and second must be numbers.' });
+    }
+    const result = first - second;
+    return res.status(200).json({ result });
 });
 
 app.use(baseUrl, baseRouter);
