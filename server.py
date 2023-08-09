662314610189c6cf239506f1240c4478e67af834
Nhithees M <98444584+NhitheesM@users.noreply.github.com>
2023-08-09 14:23:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Welcome to the Calculator API!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    if "num1" in data and "num2" in data:
+        num1 = data["num1"]
+        num2 = data["num2"]
+        result = num1 + num2
+        return jsonify({"result": result})
+    else:
+        return jsonify({"error": "Missing 'num1' or 'num2' in request data"}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    if "num1" in data and "num2" in data:
+        num1 = data["num1"]
+        num2 = data["num2"]
+        result = num1 - num2
+        return jsonify({"result": result})
+    else:
+        return jsonify({"error": "Missing 'num1' or 'num2' in request data"}), 400
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
