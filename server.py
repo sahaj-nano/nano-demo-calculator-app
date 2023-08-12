d074ca92f5325afef09b75f3806045fcfa9c3d11
Dhananjay K Jadhav <dhananjaykjadhav71@gmail.com>
2023-08-12 13:53:36 +0530

    Successfully updated endpoints, I'm Ready for Test

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hey! Sahaj, I'm Dhananjay Kisan Jadhav, from PCE Panvel."
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    num1 = request.json.get('first')
+    num2 = request.json.get('second')
+    result = num1 + num2
+    return f'{num1} + {num2} = {result}'
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    num1 = request.json.get('first')
+    num2 = request.json.get('second')
+    result = num1 - num2
+    return f'{num1} - {num2} = {result}'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
