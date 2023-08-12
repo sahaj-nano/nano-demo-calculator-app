7e5992e76ed2caab5202e8302a431cc9e54ab751
Dhananjay K Jadhav <dhananjaykjadhav71@gmail.com>
2023-08-12 16:14:46 +0530

    Successfully updated endpoints,changed result format v2, I'm Ready for Test

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return "Hey! Sahaj, I'm Dhananjay Kisan Jadhav, from PCE Panvel."
+    # return "Hey! Sahaj, I'm Dhananjay Kisan Jadhav, from PCE Panvel."
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
@@ -13,7 +14,7 @@ def add():
     num2 = request.json.get('second')
     result = num1 + num2
     # return f'{num1} + {num2} = {result}'
-    return f'{result}'
+    return f'result:{result}'
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
@@ -21,7 +22,7 @@ def subtract():
     num2 = request.json.get('second')
     result = num1 - num2
     # return f'{num1} - {num2} = {result}'
-    return f'{result}'
+    return f'result: {result}'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
