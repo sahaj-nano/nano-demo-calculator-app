f650ddf525bfb6a005aef955ebb5f9b25c60f35c
Dhananjay K Jadhav <dhananjaykjadhav71@gmail.com>
2023-08-12 14:00:01 +0530

    Successfully updated endpoints,changed result format, I'm Ready for Test

--- server.py
+++ server.py
@@ -12,14 +12,16 @@ def add():
     num1 = request.json.get('first')
     num2 = request.json.get('second')
     result = num1 + num2
-    return f'{num1} + {num2} = {result}'
+    # return f'{num1} + {num2} = {result}'
+    return f'{result}'
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     num1 = request.json.get('first')
     num2 = request.json.get('second')
     result = num1 - num2
-    return f'{num1} - {num2} = {result}'
+    # return f'{num1} - {num2} = {result}'
+    return f'{result}'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
