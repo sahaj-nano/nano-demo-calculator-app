3069b9e93d89a6a6bc447d73844bc4027ea8b43d
Banda Charith Sai <93471051+Banda-CharithSai@users.noreply.github.com>
2023-08-16 01:06:51 +0530

    test2

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     number = request.json
-    result = number['first'] - number['second']
+    result = number['first'] + number['second']
     return {"result": result }
 
 @app.route("/calculator/subtract", methods=['POST'])
