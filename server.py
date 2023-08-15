7e8b503f926ba21cee17750deb3286e8646166a6
KarthikMukkanti <karthikmukkanti128@gmail.com>
2023-08-15 21:09:31 +0530

    done

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
