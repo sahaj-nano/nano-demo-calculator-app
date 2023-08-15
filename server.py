5b650f479e4290ebefccb7df671ef96112479814
STRIZERORTIX <abhi.varadan@gmail.com>
2023-08-15 23:57:14 +0530

    changes made

--- server.py
+++ server.py
@@ -4,30 +4,35 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return "Welcome to the Calculator API!"
+    return "Hello world!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
     data = request.get_json()
-    if 'numbers' in data and isinstance(data['numbers'], list):
-        result = sum(data['numbers'])
-        return jsonify({"result": result})
+    if "first" in data and "second" in data:
+        try:
+            first = float(data["first"])
+            second = float(data["second"])
+            result = first + second
+            return jsonify({"result": result}), 200
+        except ValueError:
+            return jsonify({"error": "Invalid input. Please provide valid numbers."}), 400
     else:
-        return jsonify({"error": "Invalid input"}), 400
+        return jsonify({"error": "Missing 'first' or 'second' parameter in request body."}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     data = request.get_json()
-    if 'numbers' in data and isinstance(data['numbers'], list):
-        if len(data['numbers']) >= 2:
-            result = data['numbers'][0]
-            for num in data['numbers'][1:]:
-                result -= num
-            return jsonify({"result": result})
-        else:
-            return jsonify({"error": "At least two numbers required"}), 400
+    if "first" in data and "second" in data:
+        try:
+            first = float(data["first"])
+            second = float(data["second"])
+            result = first - second
+            return jsonify({"result": result}), 200
+        except ValueError:
+            return jsonify({"error": "Invalid input. Please provide valid numbers."}), 400
     else:
-        return jsonify({"error": "Invalid input"}), 400
+        return jsonify({"error": "Missing 'first' or 'second' parameter in request body."}), 400
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
