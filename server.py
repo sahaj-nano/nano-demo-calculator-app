bc2eec6c6bd0161a1771cbc29a851458a4519840
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2023-08-16 00:37:02 +0530

    Test #3

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return jsonify(Content="Hello world!"), 200
+    return 'hello world!', 200
+
 
 @app.route('/calculator/add', methods=['POST'])
 def add():
