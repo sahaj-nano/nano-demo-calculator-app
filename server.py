5749af0b8d68ba415b1ca57782e376c9ad92e19a
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-08-15 17:10:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    return 'a+b'
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
