2e8c3fbe848b5faef37111c847bb20ec740d8954
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2023-08-15 17:43:36 +0000

    The missing function were added.

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
-app = Flask(__name__)
-
+app = Flask(_name_)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()  # Get JSON data from the request
+    first = data.get('first', 0)
+    second = data.get('second', 0)
+    result = first + second
+    response = {'result': result}
+    return jsonify(response), 200  # Return JSON response with result and status code 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()  # Get JSON data from the request
+    first = data.get('first', 0)
+    second = data.get('second', 0)
+    result = first - second
+    response = {'result': result}
+    return jsonify(response), 200  # Return JSON response with result and status code 200
 
-if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+if _name_ == '_main_':
+    app.run(port=8080, host='0.0.0.0')
