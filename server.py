ab56069c5644a657253d1d776ce101c550465704
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2023-08-15 23:28:25 +0530

    Minor bugs fixed.

--- server.py
+++ server.py
@@ -1,28 +1,32 @@
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-@app.route("/calculator/greeting", methods=['GET'])
+@app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return jsonify(Content="Hello world!"), 200
 
-@app.route("/calculator/add", methods=['POST'])
+@app.route('/calculator/add', methods=['POST'])
 def add():
-    data = request.get_json()  # Get JSON data from the request
-    first = data.get('first', 0)
-    second = data.get('second', 0)
-    result = first + second
-    response = {'result': result}
-    return jsonify(response), 200  # Return JSON response with result and status code 200
+    try:
+        data = request.json
+        first_number = data['first']
+        second_number = data['second']
+        result = first_number + second_number
+        return jsonify(result=result), 200
+    except KeyError:
+        return jsonify(error="Invalid request format"), 400
 
-@app.route("/calculator/subtract", methods=['POST'])
+@app.route('/calculator/subtract', methods=['POST'])
 def subtract():
-    data = request.get_json()  # Get JSON data from the request
-    first = data.get('first', 0)
-    second = data.get('second', 0)
-    result = first - second
-    response = {'result': result}
-    return jsonify(response), 200  # Return JSON response with result and status code 200
+    try:
+        data = request.json
+        first_number = data['first']
+        second_number = data['second']
+        result = first_number - second_number
+        return jsonify(result=result), 200
+    except KeyError:
+        return jsonify(error="Invalid request format"), 400
 
-if _name_ == '_main_':
-    app.run(port=8080, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True)
