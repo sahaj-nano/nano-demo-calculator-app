56bdde1ed0f0b397311b670a924711ccd06450c3
Jietthesh Balaji <mljb413@gmail.com>
2023-08-15 19:48:32 +0530

    Completed the calculator application

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const baseUrl = '/calculator'
 
@@ -10,16 +10,16 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    res.json({"result": req.body.first + req.body.second});
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    res.json({"result": req.body.first - req.body.second});
 });
 
 app.use(baseUrl, baseRouter);
