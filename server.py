d014d96ac0760c59bce4336c8c3a6cdc018e2ad4
likith <likith1118@gmail.com>
2023-08-16 00:03:37 +0530

    done

--- server.py
+++ server.py
@@ -1,19 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return '<p style="color:red">hehe</p>'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    num1 = data.get('first')
+    num2 = data.get('second')
+    result = num1 + num2
+    return str(result)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    num1 = data.get('first')
+    num2 = data.get('second')
+    result = num1 - num2
+    return str(result)
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8000, host='0.0.0.0')
