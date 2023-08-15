61faf266eedc9051292d41f9aee456870dbf250d
Kinshuk2020 <kinshukgoyal2003@gmail.com>
2023-08-15 22:34:51 +0530

    nano practice

--- server.py
+++ server.py
@@ -1,19 +1,38 @@
 from flask import Flask
+from flask import json, request, Response
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    res = {
+        'result': "Hello world!",
+        'Status': 200
+    } 
+    return res
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    first = request.json.get('first')
+    second = request.json.get('second')
+    res = {
+        'result': first + second
+    }
+    return res
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    first = request.json.get('first')
+    second = request.json.get('second')
+    if(first > second):
+        x= first - second
+    else:
+        x= second - first
+    res = {
+        'result': x
+    }
+    return res
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
