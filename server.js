e8352251e82ac17289ba3de2c82b7bf695afefbf
AmanSingh6574 <amansingh6574@gmail.com>
2023-08-16 16:49:58 +0530

    Solve the isse and Updated the code

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  return res.json({
+    code: 200,
+    content: "Hello world!",
+  });
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  const { first, second } = req.body;
+  const sum = first + second;
+  res.json({
+    "Status code": 200,
+    result: sum,
+  });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  const { first, second } = req.body;
+  const sub = first - second;
+  res.json({ 
+    "Status code": 200,
+    result: sub });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
