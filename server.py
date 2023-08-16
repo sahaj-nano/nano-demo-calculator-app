75684ea98fd9c079c1548db0cc964b6734651171
Jayanth Menon S <jayanthmenons@gmail.com>
2023-08-16 08:14:13 +0530

    changes made

--- server.py
+++ server.py
@@ -1,19 +1,42 @@
-from flask import Flask
+# from flask import Flask,request,jsonify
+
+# app = Flask(__name__)
+
+
+# @app.route("/calculator/greeting", methods=['GET'])
+# def greeting():
+#     return ''
+
+# @app.route("/calculator/add", methods=['POST'])
+# def add():
+#     return ''
+
+# @app.route("/calculator/subtract", methods=['POST'])
+# def subtract():
+#     return ''
+
+# if __name__ == '__main__':
+#     app.run(port=8080,host='0.0.0.0')
+
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    res=request.get_json()
+    return jsonify(({'result': int(res['first']+res['second'])}))
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    res=request.get_json()
+    return jsonify(({'result': int(res['first']-res['second'])}))
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
