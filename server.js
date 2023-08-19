580a1ad7e50fbff1e4f6e3ef5f87eb492cd22c5f
AradhyaKC <arukc47@gmail.com>
2023-08-19 16:38:39 +0530

    Added /greeting get method and /add and /subtract post methods

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+function CheckIfParamIsNum(req, res, next) {
+  if (isNaN(req.body.first) || isNaN(req.body.second)) {
+    return res.status(400).json({ error: "Invalid parameters" });
+  }
+  next();
+}
+
+baseRouter.get("/greeting", (req, res) => {
+  return res.send("Hello world!");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", CheckIfParamIsNum, (req, res) => {
+  const { first, second } = req.body;
+  return res.json({ result: first + second });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", CheckIfParamIsNum, (req, res) => {
+  const { first, second } = req.body;
+  return res.json({ result: first - second });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
