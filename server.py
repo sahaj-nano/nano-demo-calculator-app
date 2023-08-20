cdb788944455c9a179d9a802575e621976fde958
AnanyaMG <142593445+AnanyaMG@users.noreply.github.com>
2023-08-20 17:04:40 +0530

    Server.py
    New Server edit 5_4

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/calculator/greeting", methods=['GET'])
+
 def greeting():
     return ''
 @app.route("/calculator/add", methods=['POST'])
+
 def add():
     return ''
 @app.route("/calculator/subtract", methods=['POST'])
+
 def subtract():
     return ''
 if __name__ == '__main__':
