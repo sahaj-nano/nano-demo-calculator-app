f979ad1db720b6fa32e7eeb4721c57eaae67d810
Prashanth <prashanthsai189011412@gmail.com>
2023-08-21 15:17:32 +0530

    qsdfgh

--- server.py
+++ server.py
@@ -1,29 +1,39 @@
 from flask import Flask, request, jsonify
-from dataclasses import dataclass
 
-@dataclass
+app = Flask(_name_)
+
+class Numbers:
+    def _init_(self, first, second):
+        self.first = first
+        self.second = second
+
 class Result:
-    result: int
-app = Flask(__name__)
-
+    def _init_(self, value):
+        self.result = value
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return "Hello world!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    numbers = request.json
-    response = Result(numbers['first'] + numbers['second'])
-    return jsonify(response)
+    data = request.get_json()
+    if data and 'first' in data and 'second' in data:
+        numbers = Numbers(data['first'], data['second'])
+        result = Result(numbers.first + numbers.second)
+        return jsonify(result._dict_)
+    else:
+        return jsonify(error="Invalid input data"), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    numbers = request.json
-    response = Result(numbers['first'] - numbers['second'])
-    return jsonify(response)
+    data = request.get_json()
+    if data and 'first' in data and 'second' in data:
+        numbers = Numbers(data['first'], data['second'])
+        result = Result(numbers.first - numbers.second)
+        return jsonify(result._dict_)
+    else:
+        return jsonify(error="Invalid input data"), 400
 
-if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
-
-    
+if _name_ == '_main_':
+    app.run(port=8080, host='0.0.0.0')
