d93796ddad2710749eaaf907fffc82f70797620d
Jigya Taneja <94694052+Jigya1@users.noreply.github.com>
2023-08-21 18:05:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,28 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from dataclasses import dataclass
 
+@dataclass
+class Result:
+    result: int
+    
 app = Flask(__name__)
-
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
+    numbers=request.json
+    response=Result(numbers['first']+numbers['second'])
+    return jsonify(response)
     return ''
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
+    numbers=request.json
+    response=Result(numbers['first']-numbers['second'])
+    return jsonify(response)
     return ''
 
 if __name__ == '__main__':
