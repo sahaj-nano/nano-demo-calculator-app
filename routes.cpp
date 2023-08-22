d94e0d4f496f10c31715ae8dc5038d2b61d32468
Aryaman Shetty <aryamannshetty@gmail.com>
2023-08-23 00:15:48 +0530

    solution to the task

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,19 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello World!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    crow::json::wvalue result;
+    result["result"] = input["first"].i() + input["second"].i();
+    return result;
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    crow::json::wvalue result;
+    result["result"] = input["first"].i() - input["second"].i();
+    return result;
 }
