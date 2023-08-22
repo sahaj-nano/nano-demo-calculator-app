284358dd7194b7b6a3f5913bb9a24c19a75b00b0
Adarsh275 <adarshaman2000@gmail.com>
2023-08-22 23:52:21 +0530

    Added the Implementation of add and Subtract of 2 numbers

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
-from flask import Flask
+# Name: Adarsh Kumar
+# SRN: PES2UG20CS016
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
