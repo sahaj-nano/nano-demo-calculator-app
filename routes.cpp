2f16db7152859aa784a19a03ba5679173837cef2
Yashwanth H S <yashuhs2021@gmail.com>
2023-08-23 20:11:14 +0530

    added functions

--- routes.cpp
+++ routes.cpp
@@ -14,9 +14,9 @@ crow::response add(const crow::request &req)
         return crow::response(400, "Invalid JSON format");
     }
 
-    double first = input["first"].d();
-    double second = input["second"].d();
-    double result = first + second;
+    double first_num = input["first"].d();
+    double second_num = input["second"].d();
+    double result_num = first_num + second_num;
 
     crow::json::wvalue response_json;
     response_json["result"] = result;
@@ -33,12 +33,12 @@ crow::response subtract(const crow::request &req)
         return crow::response(400, "Invalid JSON format");
     }
 
-    double first = input["first"].d();
-    double second = input["second"].d();
-    double result = first - second;
+    double first_num = input["first"].d();
+    double second_num = input["second"].d();
+    double result_num = first_num - second_num;
 
     crow::json::wvalue response_json;
-    response_json["result"] = result;
+    response_json["result"] = result ;
 
     return crow::response{response_json};
 }
