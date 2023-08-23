e4084c9511b4ec84026e4e1ca5be774e083f9123
Bhagyashree-hegde2001 <82833267+Bhagyashree-hegde2001@users.noreply.github.com>
2023-08-24 01:05:39 +0530

    Revert "Remove actual answers"
    This reverts commit 0b82ae6f609938927309a4ad9b45a81fea4600cb.

--- src/main/kotlin/com/nano/MathController.kt
+++ src/main/kotlin/com/nano/MathController.kt
@@ -2,10 +2,12 @@ package com.nano
 
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.validation.Validated
+import javax.validation.constraints.NotNull
 
 @Controller("/calculator")
 @Validated
@@ -13,16 +15,16 @@ class MathController {
 
     @Get("/greeting")
     fun greeting(): HttpResponse<String> {
-        return HttpResponse.ok("")
+        return HttpResponse.ok("Hello World!")
     }
 
     @Post("/add", produces = [MediaType.APPLICATION_JSON])
-    fun add(): HttpResponse<String> {
-        return HttpResponse.ok("")
+    fun add(@Body @NotNull numbers: Numbers): HttpResponse<String> {
+        return HttpResponse.ok("""{"result": ${numbers.first + numbers.second} }""")
     }
 
     @Post("/subtract", produces = [MediaType.APPLICATION_JSON])
-    fun subtract(): HttpResponse<String> {
-        return HttpResponse.ok("")
+    fun subtract(@Body @NotNull numbers: Numbers): HttpResponse<String> {
+        return HttpResponse.ok("""{"result": ${numbers.first - numbers.second} }""")
     }
 }
