f2f3ba752f22d9c82dda45c9030b13fabc1a25c6
likikrish <140881893+likikrish@users.noreply.github.com>
2023-08-23 16:48:45 +0000

    2

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'hello krish'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
