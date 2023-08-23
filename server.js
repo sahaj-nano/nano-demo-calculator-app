f0211cf68c1531a9aae6c9a4cb8f951e6dcbc9bc
Azeem R Gadkari <azeemgadkari5@gmail.com>
2023-08-23 19:27:49 +0530

    updated node.js

--- server.js
+++ server.js
@@ -10,16 +10,18 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.status(200).send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    res.status(200).json({ "result": first + second });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    res.status(200).json({ "result": first - second });
 });
 
 app.use(baseUrl, baseRouter);
