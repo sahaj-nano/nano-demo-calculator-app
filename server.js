8548b3541209326a30cfb99a85a2844c477f6dd9
Abhishek kumar <abhishek@Abhisheks-MacBook-Air.local>
2023-08-23 19:50:40 +0530

    final

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  return res.send("Hello world!");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  const dat = req.body;
+  let f = dat["first"];
+  let s = dat["second"];
+
+  res.json({ result: f + s });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  const dat = req.body;
+  let f = dat["first"];
+  let s = dat["second"];
+  res.json({ "": f - s });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
