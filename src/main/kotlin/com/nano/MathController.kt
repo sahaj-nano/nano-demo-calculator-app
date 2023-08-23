d8ded2dce4a094403586cf80f918751638dd8616
PRATHAM R <prathamr@PRATHAMs-MacBook-Air-2.local>
2023-08-23 22:57:37 +0530

    Implement calculator endpoints

--- src/main/kotlin/com/nano/MathController.kt
+++ src/main/kotlin/com/nano/MathController.kt
@@ -13,16 +13,24 @@ class MathController {
 
     @Get("/greeting")
     fun greeting(): HttpResponse<String> {
-        return HttpResponse.ok("")
+        return HttpResponse.ok("Hello world!")
     }
 
     @Post("/add", produces = [MediaType.APPLICATION_JSON])
-    fun add(): HttpResponse<String> {
-        return HttpResponse.ok("")
+    fun add(request: Numbers): HttpResponse<ApiResponse> {
+        val result = request.first + request.second
+        val response = ApiResponse(result)
+        return HttpResponse.ok(response)
     }
 
     @Post("/subtract", produces = [MediaType.APPLICATION_JSON])
-    fun subtract(): HttpResponse<String> {
-        return HttpResponse.ok("")
+    fun subtract(request: Numbers): HttpResponse<ApiResponse> {
+        val result = request.first - request.second
+        val response = ApiResponse(result)
+        return HttpResponse.ok(response)
     }
 }
+
+data class Numbers(val first: Double, val second: Double)
+
+data class ApiResponse(val result: Double)
