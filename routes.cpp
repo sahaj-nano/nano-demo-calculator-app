bab447e93c618e35dac4d86862813244049361fb
Yashwanth H S <yashuhs2021@gmail.com>
2023-08-23 19:48:00 +0530

    Your commit message here

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,69 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!\n"};
 }
+
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON format");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first + second;
+
+    crow::json::wvalue response_json;
+    response_json["result"] = result;
+
+    return crow::response{response_json};
 }
+
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON format");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first - second;
+
+    crow::json::wvalue response_json;
+    response_json["result"] = result;
+
+    return crow::response{response_json};
+}
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/calculator/greeting")
+    .methods("GET"_method)
+    ([] {
+        return greet();
+    });
+
+    CROW_ROUTE(app, "/calculator/add")
+    .methods("POST"_method)
+    ([](const crow::request &req) {
+        return add(req);
+    });
+
+    CROW_ROUTE(app, "/calculator/subtract")
+    .methods("POST"_method)
+    ([](const crow::request &req) {
+        return subtract(req);
+    });
+
+    app.port(8080).multithreaded().run();
+    return 0;
 }
