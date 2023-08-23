71cb65ab399a1d1a0b669b263e46e1e44db4da70
naveenreddy1289 <avnr1289@gmail.com>
2023-08-23 22:44:41 +0530

    sucess

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    fn = data["first"]
+    sn = data["second"]
+    res ={
+        "result": fn + sn
+    }
+    return res
+
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    fn = data["first"]
+    sn = data["second"]
+    res ={
+        "result": fn - sn
+    }
+    return res
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run()
