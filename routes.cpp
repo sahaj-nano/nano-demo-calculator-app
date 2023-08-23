038e412ad5a07a5d1c769fe03154074a0a343998
Yashwanth H S <yashuhs2021@gmail.com>
2023-08-23 23:37:37 +0530

    trying diff aproach

--- routes.cpp
+++ routes.cpp
@@ -24,21 +24,21 @@ crow::response add(const crow::request &req)
     return crow::response{response_json};
 }
 
-crow::response subtract(const crow::request &req){
-
+crow::response subtract(const crow::request &req)
+{
     auto input = crow::json::load(req.body);
-
-    if(!input){
+    
+    if (!input)
+    {
         return crow::response(400, "Invalid JSON format");
     }
 
     double first_num = input["first"].d();
     double second_num = input["second"].d();
-    double result_num = first_num + second_num;
+    double result_num = first_num - second_num;
 
     crow::json::wvalue response_json;
-
-    response_json["result"] = result_num;
+    response_json["result"] = result_num ;
 
     return crow::response{response_json};
 }
