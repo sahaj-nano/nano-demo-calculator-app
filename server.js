fe287b9cd4ec1234436f6bcabd6b401e2d7f151d
suriya911 <suriyachellappan@gmail.com>
2023-08-23 14:12:25 +0530

    1st try

--- server.js
+++ server.js
@@ -10,16 +10,20 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    var n1=req.body.first;
+    var n2=req.body.second;
+    res.json({ "result": n1+n2 });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    var n1=req.body.first;
+    var n2=req.body.second;
+    res.json({ "result": n1-n2 });
 });
 
 app.use(baseUrl, baseRouter);
