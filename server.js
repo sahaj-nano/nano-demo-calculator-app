f5d439f16a1aef3226b65841d6dcda61b2deb4b5
Jagadeesh <84759769+Jagadeesh-tamizh@users.noreply.github.com>
2023-08-23 06:45:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,28 +9,21 @@ app.use(express.json());
 
 const baseRouter = express.Router();
 
-app.get("/", (req, res) => {
-  res.send("This is a base url");
-});
-
-baseRouter.get("/", (req, res) => {
-  res.send("This is a calculator");
-});
 
 baseRouter.get("/greeting", (req, res) => {
-  return res.status(200).send("Hello world!");
+  return res.status(200).send("Hello World!");
 });
 
 baseRouter.post("/add", (req, res) => {
   var num1 = req.body.first;
   var num2 = req.body.second;
-  res.status(200).json({ result: `${num1 + num2}` });
+  res.status(200).json({ "result": num1 + num2 });
 });
 
 baseRouter.post("/subtract", (req, res) => {
   var num1 = req.body.first;
   var num2 = req.body.second;
-  res.status(200).json({ result: `${num1 - num2}` });
+  res.status(200).json({ "result": num1 - num2 });
 });
 
 app.use(baseUrl, baseRouter);
