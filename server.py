e5ff7b182d4a0b5da38729065ca72bd9642ccbab
Rahul-262001 <107297509+Rahul-262001@users.noreply.github.com>
2023-08-23 22:58:04 +0530

    Update server.py

--- server.py
+++ server.py
@@ -10,12 +10,12 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     data = request.get_json()		
-    return {'result:' int()data['first'])+int(data['second'])}
+    return {'result': int()data['first'])+int(data['second'])}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     data = request.get_json()
-    return {'result:' int()data['first'])-int(data['second'])}
+    return {'result': int()data['first'])-int(data['second'])}
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
