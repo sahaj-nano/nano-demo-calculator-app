728cd751dee637696c7cefed13a4a4355b7edc01
Rahul-262001 <107297509+Rahul-262001@users.noreply.github.com>
2023-08-23 20:05:20 +0530

    Update to routes

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    return {"result" : int(data['first']) + int(data['second'])}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    return {"result" : int(data['first']) - int(data['second'])}
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
