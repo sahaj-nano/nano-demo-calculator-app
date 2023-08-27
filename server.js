f35134b459e7a7e3fe7ad011ef097b25b864d926
Chandu-472 <chandanaas121@gmail.com>
2023-08-27 21:37:33 +0530

    comitted

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  res.send({
+    message: "Hey chandini",
+  });
 });
 
-baseRouter.post('/add', (req, res) => {
-    const body = req.body;
-    const first = body.first;
-    const second = body.second;
-    const result = first + second;
-    res.send({
-        result: result
-    })
+baseRouter.post("/add", (req, res) => {
+  const body = req.body;
+  const first = body.first;
+  const second = body.second;
+  const result = first + second;
+  res.send({
+    result: result,
+  });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  res.json({ "": null });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
