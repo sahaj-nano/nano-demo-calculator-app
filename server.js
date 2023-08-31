85e0572ec477b42bc9d921efdd41680aaf968d1c
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2023-08-31 22:26:59 +0530

    done

--- server.js
+++ server.js
@@ -10,23 +10,27 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.status(200).send('Hello World');
+    return res.status(200).send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
 
-    // console.log(req.body);
-    const {first, second} = req.body;
-
-    console.log(first,second);
-
-    res.status(200).json({ "result": first+second });
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both numbers are required.' });
+    }
+    const result = first + second;
+    res.status(200).json({ "result": result });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    const {first, second} = req.body;
-    res.json({ "result": first-second });
+    const { first, second } = req.body;
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both numbers are required.' });
+    }
+    const result = first - second;
+    res.status(200).json({ "result": result });
 });
 
 app.use(baseUrl, baseRouter);
