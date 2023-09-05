937475e5e43372605d8476a092e41277e2296c00
Shaileshhariharan03 <57766024+Shaileshhariharan03@users.noreply.github.com>
2023-09-05 22:23:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello world!", 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first + second
+        response = {'result': result}
+        return jsonify(response), 200
+    else:
+        return jsonify({'error': 'Invalid input'}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first - second
+        response = {'result': result}
+        return jsonify(response), 200
+    else:
+        return jsonify({'error': 'Invalid input'}), 400
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
