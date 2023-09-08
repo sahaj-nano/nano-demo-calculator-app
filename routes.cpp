ad4ad7c54ac1e017f686164b04a76cb6a62b6a7a
Faizunnisa-Mogal <144457171+Faizunnisa-Mogal@users.noreply.github.com>
2023-09-08 20:19:23 +0530

    Update routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,27 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if(!input){
+        return crow::response(200,"INVALID JSON");
+    }
+    int num1=input["num1"].i();
+    int num2=input["num2"].i();
+    int res=num1+num2;
+    return crow::response{crow::json::dump_to_string(res)};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+     if(!input){
+        return crow::response(200,"INVALID JSON");
+    }
+    int num1=input["num1"].i();
+    int num2=input["num2"].i();
+    int res=num1-num2;
+    return crow::response{crow::json::dump_to_string(res)};
 }
