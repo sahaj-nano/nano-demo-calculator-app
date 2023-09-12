9267b183ab14ceb6224d958760d543d20d3dc3fe
hari-7707 <106306395+hari-7707@users.noreply.github.com>
2023-09-12 21:08:18 +0530

    hari

--- server.py
+++ server.py
@@ -5,15 +5,20 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
+   
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+     numbers = request.json
+     response = Result(numbers['first'] - numbers['second'])
+     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
