3119d77c92e1c36133bfb7b3e1c9002f7d1fce2d
chandra <chandrasai681@gmail.com>
2023-09-14 22:30:31 +0530

    updated the endpoints

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000
 
 const baseUrl = '/calculator'
 
@@ -14,12 +14,22 @@ baseRouter.get('/greeting', (req, res) => {
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    var a=parseInt(req.body.first);
+     var b=parseInt(req.body.second);
+     res.send({
+        "result":a+b,
+     }
+     ).json();
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    var a=parseInt(req.body.first);
+    var b=parseInt(req.body.second);
+    res.send({
+       "result":a-b,
+    }
+    ).json();
 });
 
 app.use(baseUrl, baseRouter);
