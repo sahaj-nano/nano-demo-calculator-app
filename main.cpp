4e5cadc5a395eee56c9f5ec2a5d24d8f8dcf59ef
sai <119105780+sai-kumar2413@users.noreply.github.com>
2023-09-14 14:06:40 +0000

    updated c++

--- main.cpp
+++ main.cpp
@@ -7,16 +7,12 @@ int main()
     
 
   crow::SimpleApp app;
-  statusCode = apiContractObject.GetStatusCode();
-  cout << "Code: " << statusCode << endl
   CROW_ROUTE(app, "/calculator/greeting")
       .methods("GET"_method)(&greet);
 
-    cout << "Status code: " << statusCode << endl;
   CROW_ROUTE(app, "/calculator/add")
       .methods("POST"_method)(&add);
       
-      cout << "Status code: " << statusCode << endl;
   CROW_ROUTE(app, "/calculator/subtract")
       .methods("POST"_method)(&subtract);
 
