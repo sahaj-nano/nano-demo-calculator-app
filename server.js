b83e2119b1839fd2f9a9758390760c60780ce2ab
GitVishal08 <paindlavishal08@gmail.com>
2023-09-15 01:08:12 +0530

    solved all the calculator app endpoints

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const baseUrl = '/calculator'
 
@@ -10,16 +10,24 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('service is availble');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
-});
+     var num1=parseInt(req.body.first);
+     var num2=parseInt(req.body.second);
+     res.send({
+        result:num1+num2,
+     })
+}); 
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    var num1=parseInt(req.body.first);
+     var num2=parseInt(req.body.second);
+     res.send({
+        result:num1-num2,
+     })
 });
 
 app.use(baseUrl, baseRouter);
