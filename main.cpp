8c19e6d7ca9a23d0614341e0f5db3a4cc85a9555
sai <119105780+sai-kumar2413@users.noreply.github.com>
2023-09-14 13:50:18 +0000

    updated c++

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 int main()
 {
-    string statusCode;
+    
 
   crow::SimpleApp app;
   statusCode = apiContractObject.GetStatusCode();
--- routes.cpp
+++ routes.cpp
@@ -4,16 +4,23 @@ crow::response greet()
 {
     
     return crow::response{"Hello world!"};
+    return crow::response{""};
 }
 crow::response add(const crow::request &req)
 {
     
     auto input = crow::json::load(req.body);
-    return crow::response{"result ":req.body.first+req.body.second};
+     crow::json::wvalue result;
+    result["result"] = input["first"].i() + input["second"].i();
+    return result;
+    return crow::response{""};
 }
 crow::response subtract(const crow::request &req)
 {
     
     auto input = crow::json::load(req.body);
-    return crow::response{"result ":req.body.first+req.body.second};
+    crow::json::wvalue result;
+    result["result"] = input["first"].i() - input["second"].i();
+    return result;
+    return crow::response{""};
 }
