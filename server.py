88c72d3a47f302070ec9065452841cfaf5d92d67
Mahender <75716439+MahenderPoshaboina@users.noreply.github.com>
2023-09-14 12:00:01 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,32 @@
 from flask import Flask, request, jsonify
-from dataclasses import dataclass
-
-@dataclass
-class Result:
-    result: int
-
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return "Hello world!", 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    numbers = request.json
-    response = Result(numbers['first'] + numbers['second'])
-    return jsonify(response)
+    try:
+        data = request.get_json()
+        first = data["first"]
+        second = data["second"]
+        result = first + second
+        return jsonify({"result": result}), 200
+    except KeyError:
+        return "Invalid request data", 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    numbers = request.json
-    response = Result(numbers['first'] - numbers['second'])
-    return jsonify(response)
+    try:
+        data = request.get_json()
+        first = data["first"]
+        second = data["second"]
+        result = first - second
+        return jsonify({"result": result}), 200
+    except KeyError:
+        return "Invalid request data", 400
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
