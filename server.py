069f6d5b01ead396a756ca4d72874eff05022456
Mahender <75716439+MahenderPoshaboina@users.noreply.github.com>
2023-09-14 14:08:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -10,17 +10,18 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     data = request.get_json()
-    f= numbers['first']
-    s= numbers['second']
+    f= numbers["first"]
+    s= numbers["second"]
     res= f+s
-    return jsonify({"result": result}), 200
+    return jsonify({"result": res}), 200
+
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     numbers = request.get_json()
-    f= numbers['first']
-    s= numbers['second']
+    f= numbers["first"]
+    s= numbers["second"]
     res= f-s
-    return jsonify({"result": result}), 200
+    return jsonify({"result": res}), 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
