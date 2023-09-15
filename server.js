474d0f24b9ae1e05dbe465c3e16aad9e17df8131
chandra <chandrasai681@gmail.com>
2023-09-15 10:56:55 +0530

    port changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 const baseUrl = '/calculator'
 const bodyparser = require("body-parser");
