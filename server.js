2c77c3c7aabf8e0fdfc59b288d4dba5e01ece271
asvparthasarathi <asvparthasarathi@gmail.com>
2023-09-15 20:29:22 +0530

    commit1

--- server.js
+++ server.js
@@ -10,19 +10,23 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello world!');
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post('/add', (req, res) => {3
+    const a1=parseInt(req.body.first);
+    const a2=parseInt(req.body.second);
+    res.json({ "result": a1+a2 });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const a=parseInt(req.body.first);
+    const b=parseInt(req.body.second);
+    res.json({ "result": a-b });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+    console.log("Server running at PORT", PORT);
 });
