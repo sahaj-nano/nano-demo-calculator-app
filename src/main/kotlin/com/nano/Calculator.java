f5e5fb149badfa42d5af43993d331a850d7489c9
Joshna-josh <109268806+Joshna-josh@users.noreply.github.com>
2023-09-15 11:00:01 +0530

    Update Calculator.java

--- src/main/kotlin/com/nano/Calculator.java
+++ src/main/kotlin/com/nano/Calculator.java
@@ -1,43 +1,44 @@
-package calculator_application;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 
-import java.util.Scanner;
+import com.google.gson.Gson;
 
-public class Calculator{
-	public static String greetings(String greeting) {
-		return greeting;
-	}
-	public static int add(int a,int b) {
-		return a+b;
-	}
-	public static int sub(int a,int b) {
-		return a-b;
-	}
+public class JsonToJava {
 
+    public static void main(String[] args) throws IOException {
+        // Get the path to the JSON file.
+        String pathToJsonFile = "collections/calculator-tests.postman_collection.json";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter first number");
-		int first_number =sc.nextInt();
-		System.out.println("enter second number");
-		int second_number=sc.nextInt();
-		System.out.println("Enter Greetings");
-		String greeting = sc.nextLine();
-		System.out.println("Choose any number[0,1,2] to perform addition and subtraction");
-		int n= sc.nextInt();
+        // Create a File object for the JSON file.
+        File jsonFile = new File();
 
-		switch(n) {
-		case 0:
-			System.out.println(greetings(greeting));
-		case 1: 
-			System.out.println(add(first_number,second_number));
-			break;
-		case 2: 
-			System.out.println(sub(first_number,second_number));
-			break;
-		default:
-			System.out.println("please provide valid number");
-		}
-	}
+        // Create a Reader object for the JSON file.
+        Reader reader = new InputStreamReader(jsonFile, StandardCharsets.UTF_8);
 
+        // Create a Gson object.
+        Gson gson = new Gson();
+
+        // Convert the JSON file to a Java object.
+        Object jsonObject = gson.fromJson(reader, Object.class);
+
+        // Get the first number from the JSON object.
+        int firstNumber = Integer.parseInt((String) jsonObject.get("firstNumber"));
+
+        // Get the second number from the JSON object.
+        int secondNumber = Integer.parseInt((String) jsonObject.get("secondNumber"));
+
+        // Perform addition.
+        int sum = firstNumber + secondNumber;
+
+        // Perform subtraction.
+        int difference = firstNumber - secondNumber;
+
+        // Print the results.
+        System.out.println("The sum is " + sum);
+        System.out.println("The difference is " + difference);
+    }
 }
