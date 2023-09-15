2698b938e7b74a18a2f5a28553844909e039ac65
Sandeep-ambati <87816053+Sandeep-ambati@users.noreply.github.com>
2023-09-15 17:13:18 +0000

    cpp calculator

--- routes.cpp
+++ routes.cpp
@@ -1,16 +1,37 @@
-#include "crow_all.h"
+#include <httplib.h>
 
-crow::response greet()
-{
-    return crow::response{""};
-}
-crow::response add(const crow::request &req)
-{
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
-}
-crow::response subtract(const crow::request &req)
-{
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
+using namespace httplib;
+
+const int PORT = 8080;
+const std::string baseUrl = "/calculator";
+
+int main() {
+    Server server;
+
+    server.Get((baseUrl + "/greeting").c_str(), [](const Request& req, Response& res) {
+        res.status = 200;
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    server.Post((baseUrl + "/add").c_str(), [](const Request& req, Response& res) {
+        res.status = 200;
+        int first = std::stoi(req.get_param_value("first"));
+        int second = std::stoi(req.get_param_value("second"));
+        int result = first + second;
+        res.set_content(std::to_string(result), "application/json");
+    });
+
+    server.Post((baseUrl + "/subtract").c_str(), [](const Request& req, Response& res) {
+        res.status = 200;
+        int first = std::stoi(req.get_param_value("first"));
+        int second = std::stoi(req.get_param_value("second"));
+        int result = first - second;
+        res.set_content(std::to_string(result), "application/json");
+    });
+
+    server.listen("0.0.0.0", PORT);
+
+    std::cout << "Server running at PORT " << PORT << std::endl;
+
+    return 0;
 }
