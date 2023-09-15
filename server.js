328a7b50dcb642c5c39f49180d201fa8e0899057
chandra <chandrasai681@gmail.com>
2023-09-15 10:59:55 +0530

    helloworld

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('Hello!');
+    return res.send('hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
