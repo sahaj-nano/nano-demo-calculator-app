e11bb0633ed5f8ea4f477b881b77adeb696d789d
BijjamPrem2000 <144934543+BijjamPrem2000@users.noreply.github.com>
2023-09-16 08:40:52 +0530

    new

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
 
+#dataclasses is used to import the __init__,__repr__
 from flask import Flask, jsonify, request
 
-
-@dataclass
-class Result:
-    result: int
+#Flask is used to create a new web application
+#jsonify is used to convert the data to JSON format before sending it to the http
+#request is the data in the JSON format we convert into required format using functions request.json,request.args,request.forms
+#@dataclass
+#class Result:
+#    result: int
 
 
 app = Flask(__name__)
@@ -16,13 +19,13 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     numbers = request.json
-    response = Result(numbers['first'] + numbers['second'])
+    response = int(numbers['first'] + numbers['second'])
     return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     numbers = request.json
-    response = Result(numbers['first'] - numbers['second'])
+    response = int(numbers['first'] - numbers['second'])
     return jsonify(response)
 
 if __name__ == '__main__':
